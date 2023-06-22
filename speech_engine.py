a4eb45a5f60eed0bad6e7eeebff4d9a8972d1a7d
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-06-22 13:39:44 +0530

    Delete speech_engine.py

--- speech_engine.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def casper_speak(speak, voice):
-    engine = pyttsx3.init("sapi5")
-    engine.setProperty("rate", 150)
-    voices = engine.getProperty('voices')
-    engine.setProperty("voice", voices[voice].id)
-    engine.say(speak)
-    engine.runAndWait()
